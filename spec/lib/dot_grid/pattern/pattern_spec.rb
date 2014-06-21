816d659ac8954993cab45a2b2ae57350fe99cbb5
slabounty <slabounty@gmail.com>
2014-06-21 15:41:12 -0700

    Added Tests for Pattern

---
+++ spec/lib/dot_grid/pattern/pattern_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe "DotGrid::Pattern::Pattern" do
+
+  let(:pdf) { double('pdf') }
+  let(:params) { { pdf: pdf, bounds: double('bounds', { height: 20.mm, width: 10.mm } ), spacing: 5 } }
+  let(:subject) { DotGrid::Pattern::Pattern.new(params) }
+
+  describe "#new" do
+    it "calls the post initialize method" do
+      expect_any_instance_of(DotGrid::Pattern::Pattern).to receive(:post_initialize)
+      DotGrid::Pattern::Pattern.new(params)
+    end
+  end
+
+  describe "#post_initialize" do
+    it "is nil" do
+      expect(subject.post_initialize({})).to be_nil
+    end
+  end
+
+  describe "#rows" do
+    it "calculates the rows based on the bounds" do
+      expect(subject.rows).to eq(4)
+    end
+  end
+
+  describe "#columns" do
+    it "calculates the rows based on the bounds" do
+      expect(subject.columns).to eq(2)
+    end
+  end
+
+  describe "#draw" do
+    it "raises an error" do
+      expect { subject.draw }.to raise_error
+    end
+  end
+end
