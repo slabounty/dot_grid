581f518ff7155e4a3fe661b26d9c3182b770041d
slabounty <slabounty@gmail.com>
2014-07-12 14:55:08 -0700

    Refactored Grid Draw

--- lib/dot_grid/pattern/grid.rb
+++ lib/dot_grid/pattern/grid.rb
@@ -1,15 +1,23 @@
 module DotGrid
   module Pattern
     class Grid < Pattern
+      def draw_rows
+        (0..rows).each do |row|
+          pdf.stroke_horizontal_line(0, bounds.width, :at => row*spacing)
+        end
+      end
+
+      def draw_columns
+        (0..columns).each do |column|
+          pdf.stroke_vertical_line(0, bounds.height, :at => column*spacing)
+        end
+      end
+
       def draw
         pdf.stroke_color grid_color
         pdf.bounding_box(bounds.upper_left, width: bounds.width, height: bounds.height) do
-          (0..rows).each do |row|
-            pdf.stroke_horizontal_line(0, bounds.width, :at => row*spacing)
-          end
-          (0..columns).each do |column|
-            pdf.stroke_vertical_line(0, bounds.height, :at => column*spacing)
-          end
+          draw_rows
+          draw_columns
         end
       end
     end
