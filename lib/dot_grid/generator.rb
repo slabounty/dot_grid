5e7bb3e459b9a4df558e39c2a070a2e005e5e3b9
slabounty <slabounty@gmail.com>
2014-07-26 14:28:03 -0700

    Add Ability to Specify Page Size
    Added the ability to parse things like 13.3x5.4in in the page size.

--- lib/dot_grid/generator.rb
+++ lib/dot_grid/generator.rb
@@ -14,12 +14,16 @@ module DotGrid
 
     def initialize(params)
       @file_name = params[:file_name] || "dotgrid.pdf"
-      @page_size = params[:page_size] || "LETTER"
-      @margin = params[:margin] || 0.5
+      @page_size = params[:page_size] ? parse_page_size(params[:page_size]) : "LETTER"
+      @margin = params[:margin] || 0.5.mm
       @page_types = params[:page_types] ? params[:page_types].split(",") : ["planner"]
       @pdf = Prawn::Document.new(margin: margin, page_size: page_size, skip_page_creation: true)
-      params[:pdf] = pdf
-      @pages = create_pages(params)
+      @pages = create_pages(params.merge({pdf: pdf}))
+    end
+
+    def parse_page_size(page_size)
+      return page_size unless p = /(?'w'\d+\.{0,1}\d*)x(?'h'\d+\.{0,1}\d*)(?'u'[a-z]{2})/.match(page_size)
+      return [p[:w].to_f.send(p[:u]), p[:h].to_f.send(p[:u])]
     end
 
     def create_pages(params)
