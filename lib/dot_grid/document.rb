17458d126b6ffa0ed59bcf24443d2298ccb98657
slabounty <slabounty@gmail.com>
2014-08-12 20:22:48 -0700

    Fixed Issue with Default
    Made it so the default dot_grid would work with no parameters passed in.

--- lib/dot_grid/document.rb
+++ lib/dot_grid/document.rb
@@ -16,7 +16,7 @@ module DotGrid
       @page_size = params[:page_size] ? parse_page_size(params[:page_size]) : "LETTER"
       @orientation = params[:orientation] ? params[:orientation].downcase.to_sym : :portrait
       @margin = params[:margin] || 0.0
-      @page_types = params[:page_types] ? params[:page_types].split(",") : ["planner"]
+      @page_types = params[:page_types] ? params[:page_types].downcase.split(",").map { |w| w.strip } : ["planner"]
 
       @pdf = Prawn::Document.new(margin: margin, page_size: page_size, skip_page_creation: true, page_layout: orientation)
       @pages = create_pages(params.merge({pdf: pdf}))
@@ -29,7 +29,7 @@ module DotGrid
 
     def create_pages(params)
       page_types.map do |p|
-        DotGrid::Page::Factory.build(p.strip, params.clone)
+        DotGrid::Page::Factory.build(p, params.clone)
       end
     end
 
